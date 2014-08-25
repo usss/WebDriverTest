38706fc0e71d4fa4169940d9cde5c491faeb4725
unknown <VNKorchagin@MS-D40519-08.bee.vimpelcom.ru>
2014-08-25 18:15:08 +0400

    first pull

---
+++ src/LoginTest.java
@@ -0,0 +1,22 @@
+/**
+ * Created by VNKorchagin on 25.08.2014.
+ */
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LoginTest {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://dev-web01:23001/login.html");
+        WebElement loginForm = driver.findElement(By.id("loginFormB2C:loginForm:login"));
+        loginForm.sendKeys("Telco1");
+        WebElement passwordForm = driver.findElement(By.id("loginFormB2C:loginForm:password"));
+        passwordForm.sendKeys("unix1111");
+        WebElement loginButton = driver.findElement(By.id("loginFormB2C:loginForm:loginButton"));
+        loginButton.click();
+        /*driver.quit();*/
+    }
+}
